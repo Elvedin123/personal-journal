b62ab5bb3a1aadc46b3f85667a77a17c8212a388
elvedin123 <elvedinc97@gmail.com>
2022-01-13 15:49:22 -0500

    added a new component and form component

---
+++ client/src/components/Form.jsx
@@ -0,0 +1,9 @@
+
+export default function Form(props) {
+  const { title, topic, info, date } = props.input
+  return (
+    <div>
+
+    </div>
+  )
+}
---
+++ client/src/components/New.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+import api from "../services/apiConfig.js"
+import { useNavigate } from "react-router-dom"
+const default_input = {
+  title: "",
+  topic: "",
+  info: "",
+
+}
+export default function New() {
+  return (
+    <div>
+
+    </div>
+  )
+}
