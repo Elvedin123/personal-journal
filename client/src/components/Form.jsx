7189a98c3b6012b4e8a9870715d2be68b0ed612b
elvedin123 <elvedinc97@gmail.com>
2022-01-17 16:03:48 -0500

    finished mobile styling

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -4,34 +4,35 @@ export default function Form(props) {
   const { title, topic, info } = props.input
   // console.log(props.value.tolocalestring())
   return (
-    <form onSubmit={props.handleSubmit}>
-      <label>Title</label>
-      <input
-        id="title"
-        value={title}
-        placeholder="Add a Title"
-        onChange={props.handleTextInput}>
-      </input>
+    <div className='form-container'>
+      <form className='form' onSubmit={props.handleSubmit}>
+        <label>Title</label>
+        <input
+          id="title"
+          value={title}
+          placeholder="Add a Title"
+          onChange={props.handleTextInput}>
+        </input>
 
-      <label>Topic</label>
-      <input
-        id="topic"
-        value={topic}
-        placeholder="Add a Topic"
-        onChange={props.handleTextInput}>
-      </input>
+        <label>Topic</label>
+        <input
+          id="topic"
+          value={topic}
+          placeholder="Add a Topic"
+          onChange={props.handleTextInput}>
+        </input>
 
-      <label>Info</label>
-      <input
-        id="info"
-        value={info}
-        placeholder="Add your Info"
-        onChange={props.handleTextInput}>
-      </input>
-      <label>Date</label>
-      <Calendar value={props.value} onChange={props.onChange} />
-      <button>{props.type}</button>
-    </form>
-
+        <label>Info</label>
+        <input
+          id="info"
+          value={info}
+          placeholder="Add your Info"
+          onChange={props.handleTextInput}>
+        </input>
+        <label>Date</label>
+        <Calendar value={props.value} onChange={props.onChange} />
+        <button>{props.type}</button>
+      </form>
+    </div>
   )
 }
